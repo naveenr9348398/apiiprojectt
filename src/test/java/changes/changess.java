4cd7b68793818001dffc2445c4e1bb579a34baee
naveenr9348398 <naveen.rajasekaran74@gmail.com>
2024-11-13 15:29:37 +0530

    newcode

---
+++ src/test/java/changes/changess.java
@@ -0,0 +1,12 @@
+package changes;
+
+public class changess {
+	
+	public static void main(String[] args) {
+		System.out.println("changes1");
+		System.out.println("changes2");
+		System.out.println("changes3");
+		System.out.println("changes4");
+	}
+
+}
